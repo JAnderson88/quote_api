b365e56db2f986f1646b402e7d11de060a066256
JAnderson88 <jande081@fiu.edu>
2018-10-24 20:41:41 -0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Quote API
+
+/all 
+--> Returns the list of all the quotes from the API
+
+/random
+--> Returns a random quote from the API
+
+/:id
+--> Provide an id to the url params and it will return the quote with the corresponding ID.
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get("/all", (req, res) => {
+  const quotes = require("./quotes");
+  res.send(quotes());
+})
+
+app.get("/random", (req, res) => {
+  const quotes = require("./quotes");
+  const random = Math.floor(Math.random() *quotes().length);
+  const data = quotes()[random];
+  res.send(data); 
+});
+
+app.get("/:id", (req, res) => {
+  const quotes = require("./quotes");
+  res.send(quotes()[parseInt(req.params.id) - 1]);
+});
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
