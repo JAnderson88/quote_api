61bfe9e122175d584611f242ded821cf8f7023e5
JAnderson88 <jande081@fiu.edu>
2018-10-27 12:09:08 -0400

    added cors module for cross origin compatibility

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.all('/', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
